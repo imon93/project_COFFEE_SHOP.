19f520a51ed8e577ff101678ac7df0acd89039bc
MD. SABBIR HOSEN IMON <151059564+imon93@users.noreply.github.com>
2024-12-01 11:17:47 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+
+    $(".back_to_top").click(function() {
+        $("html, body").animate({scrollTop: 0}, 500)
+    });
+
+    $(window).on("load", function() {
+        $(".preloader_area").fadeOut(1000)
+    });
+
+    $(window).scroll(function(){
+        var scrolling = $(this).scrollTop()
+
+        if (scrolling > 200) {
+            $(".navigation").addClass("lightColor")
+            $(".back_to_top").fadeIn(500)
+        }
+        else{
+            $(".navigation").removeClass("lightColor")
+            $(".back_to_top").fadeOut(500)
+        }
+    });
+});
+
 
 
 
@@ -63,31 +87,3 @@ document.addEventListener("DOMContentLoaded", function(event){
     });
 });
 
-
-
-   
-
-
-
-
-// $(document).ready(function() {
-
-//     $(window).on("load", function() {
-//         $(".preloader_area").fadeOut(1000)
-//     });
-
-//     $(".back_to_top").click(function() {
-//         $("html, body").animate({scrollTop: 0}, 500)
-//     });
-
-//     $(window).scroll(function(){
-//         var scrolling = $(this).scrollTop()
-
-//         if (scrolling > 200) {
-//             $(".back_to_top").fadeIn(500)
-//         }
-//         else{
-//             $(".back_to_top").fadeOut(500)
-//         }
-//     });
-// });
